9753686a909caea2c931bc60160d30d3acbdde1d
eryx <omerbar7193@gmail.com>
2023-01-22 16:58:21 +0200

    Change the name of the folder to client
    Signed-off-by: eryx <omerbar7193@gmail.com>

--- SocialDev/client/src/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { authSlice } from "stateStore/auth/slices/authSlice";
-
-export default configureStore({
-	reducer: {
-		auth: authSlice.reducer,
-	},
-	middleware: (getDefauktMiddleware) => getDefauktMiddleware(),
-});
